58fa0a42c519f6139dea80a7be0e494b4305ed14
Rez27 <kadam09rohan@gmail.com>
2024-03-16 00:33:33 +0000

    Making Transaction models in Payapp

--- payapp/models.py
+++ payapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from register.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    CURRENCY_CHOICES = [
+        ('GBP', 'British Pound'),
+        ('USD', 'US Dollar'),
+        ('EUR', 'Euro'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='payapp_profile')
+    bal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='GBP')
+
+    def __str__(self):
+        return self.user.username
+
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(UserProfile, related_name='sent_transactions', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserProfile, related_name='received_transactions', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.user.username} -> {self.receiver.user.username}: {self.amount}"
