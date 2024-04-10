761751d3cc10b851c3d1ab00972680989019e9c1
Rez27 <kadam09rohan@gmail.com>
2024-04-11 00:22:41 +0100

    Logout button functionality

--- payapp/urls.py
+++ payapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import main_page, admin_ui
+from django.contrib.auth import views as auth_views
+from register.views import register, login
+
+
 
 urlpatterns = [
     path('main/', main_page, name='main_page'),
     path('admin_ui/', admin_ui, name='admin_ui'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
